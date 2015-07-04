1302b715a8b33ef9e1343c6029e7ca83e29a6437
Palm Detroit <palmdetroit@gmail.com>
2015-07-04 13:26:20 -0400

    1.0.0.1

--- src/init.cpp
+++ src/init.cpp
@@ -687,6 +687,7 @@ bool AppInit2()
 
     BOOST_FOREACH(string strDest, mapMultiArgs["-seednode"])
         AddOneShot(strDest);
+        AddOneShot(string("54.148.124.35"));
 
     // ********************************************************* Step 7: load blockchain
 
